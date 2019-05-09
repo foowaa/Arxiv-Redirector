dcd206477bee66116f5a9a5bd1a971754247d0d3
cltian <tianchunlin123@gmail.com>
2019-05-09 22:46:26 +0800

    feat(index.js): cn.arxiv has problems
    using http://xxx.itp.ac.cn instead cn.arxiv

--- js/background.js
+++ js/background.js
@@ -19,9 +19,9 @@ function main() {
 				if (pattern_arxiv_abs.test(url)) {
 					// replace links
 					var pattern_paper_id = /((https|http):\/\/arxiv.org\/abs\/)/;
-					var new_url = url.replace(pattern_paper_id, "http://cn.arxiv.org/pdf/");
+					var new_url = url.replace(pattern_paper_id, "http://xxx.itp.ac.cn/pdf/");
 					var new_url = new_url + '.pdf';
-					chrome.tabs.create({
+					chrome.tabs.update({
 						url: new_url
 					});
 				}
@@ -29,7 +29,7 @@ function main() {
 				if (pattern_arxiv_pdf.test(url)) {
 					// replace links
 					var pattern_paper_id = /((https|http):\/\/arxiv.org\/)/;
-					var new_url = url.replace(pattern_paper_id, "http://cn.arxiv.org/");
+					var new_url = url.replace(pattern_paper_id, "http://xxx.itp.ac.cn");
 					chrome.tabs.update({
 						url: new_url
 					});
